121bd02ab19b81846d7cfadf9bd37973d1cecd11
Guilherme <guilherme.benedito@fatec.sp.gov.br>
2024-10-05 18:05:57 -0300

    Adicionando estilo

--- listaIII/app.js
+++ listaIII/app.js
@@ -5,6 +5,7 @@ var app = express()
 app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.json())
+app.use(express.static('public'))
 
 let tarefas = []
 
